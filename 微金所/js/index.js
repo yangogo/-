851cb326f45cb74d4bc08c40f1628ed9ef81e4e4
yangogo <3135886916@qq.com>
2018-07-10 20:30:35 +0800

    微金所

---
+++ 微金所/js/index.js
@@ -0,0 +1,20 @@
+whatScreen();
+
+// 屏幕的宽度改变之后 会自动触发
+onresize=function () {
+  whatScreen();
+  
+}
+function whatScreen() {
+  var width = document.querySelector("html").offsetWidth;
+  var title = document.querySelector("title");
+  if (width <= 768) {
+      title.innerHTML = "极小屏幕-" + width;
+  } else if (width > 768 && width <= 992) {
+      title.innerHTML = "小屏幕-" + width;
+  } else if (width > 992 && width <= 1200) {
+      title.innerHTML = "普通屏幕-" + width;
+  } else if (width > 1200) {
+      title.innerHTML = "大屏幕-" + width;
+  }
+}
